b583ed4fe9f814559a2e616e7eeb03e93bad4010
Srdja263 <40635431+Srdja263@users.noreply.github.com>
2018-12-05 12:06:16 +0100

    Add files via upload
    Gotov zadatak 2

---
+++ Pred1Zad2/src/Calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculator {
+       public static void main(String args[]) {
+    	double num1,num2,result;
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Unesi prvi broj");  
+        num1=scanner.nextDouble();
+        System.out.println("Unesi drugi broj"); 
+        num2=scanner.nextDouble();
+       
+        System.out.println("Unesi kod operacije (s-sabiranje,o-oduzimanje,m-mnozenje,d-deljenje)");
+        Scanner scanner1 = new Scanner(System.in);
+       
+    	char kod = scanner1.next().charAt(0);
+    	 scanner.close();
+    	 scanner1.close();
+    	 switch(kod)
+    	 {
+    	 case 's':
+    		 result=num1+num2;
+    		 break;
+    	 case 'o':
+    		 result=num1-num2;
+    	     break;
+    	 case 'm':
+    		 result=num1*num2;
+    		 break;
+    	 case 'd':
+    		 result=num1/num2;
+    	     break;
+    	     default:
+    	    	 System.out.println("Uneli ste pogresan kod");
+    	    	 return;
+    	     
+    	 }
+    	 System.out.println(result);
+       }
+}
